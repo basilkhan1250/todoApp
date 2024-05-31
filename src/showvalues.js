fd1eaaa0a9f1a739f64e4fe0dc557536e94a5a08
basilkhan1250 <basilkhan1250@gmail.com>
2024-05-31 16:13:48 -0700

    todoReact

---
+++ src/showvalues.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export function Showvalues(props) {
+    const value = props.task;
+    console.log(value);
+    return (
+        <>
+            {value.map((x, i) => (
+                <ul className="list-disc list-inside mt-5 text-lg mr-72" key={i}>
+                    <li className="my-3">{x}</li>
+                    <button
+                        onClick={(e =>{
+                           e.target.parentNode.remove()
+                        })} className="bg-rose-300 py-1 px-4 rounded-lg ml-5 hover:brightness-110 active:brightness-90">Delete</button>
+                    <button
+                    
+                    className="bg-rose-300 py-1 px-4 rounded-lg ml-5 hover:brightness-110 active:brightness-90">Edit</button>
+                </ul>
+            ))}
+        </>
+    );
+}
+
+
